5e1db1ea356aaf8a123271590fa0a45336052949
tjddnjs0102 <rjf654@gmail.com>
2023-07-27 13:11:25 +0900

    first commit

---
+++ src/main/java/com/example/practiceci/PracticeCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practiceci/PracticeCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
